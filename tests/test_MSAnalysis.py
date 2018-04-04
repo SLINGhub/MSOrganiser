d52e2ca566a04c12276c5fb73672ef7440c80b21
Jeremy John Selva <bchjjs@nus.edu.sg>
2018-04-04 11:38:34 +0800

    Add new test data and new test units. Will proceed to cleaning up the code.

--- tests/test_MSAnalysis.py
+++ tests/test_MSAnalysis.py
@@ -9,15 +9,29 @@ WIDETABLEFORM_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'Wid
 WIDETABLEFORM_ANNOTATION = os.path.join(os.path.dirname(__file__),"testdata", 'WideTableForm_Annotation.xlsm')
 WIDETABLEFORM_RESULTS_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'WideTableForm_Results.xlsx')
 
+LARGE_WIDETABLEFORM_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'sPerfect_Index_AllLipids_raw.csv')
+LARGE_WIDETABLEFORM_ANNOTATION = os.path.join(os.path.dirname(__file__),"testdata", 'sPerfect_Index_AllLipids_raw_Annotation.xlsm')
+LARGE_WIDETABLEFORM_RESULTS_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'sPerfect_Index_AllLipids_raw_Results.xlsx')
+
+COMPOUNDTABLEFORM_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'CompoundTableForm.csv')
+COMPOUNDTABLEFORM_ANNOTATION = os.path.join(os.path.dirname(__file__),"testdata", 'CompoundTableForm_Annotation.xlsm')
+COMPOUNDTABLEFORM_RESULTS_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'CompoundTableForm_Results.xlsx')
+
 class Agilent_Test(unittest.TestCase):
 
     def setUp(self):
-        self.MyData = MS_Analysis(WIDETABLEFORM_FILENAME)
+        self.MyWideData = MS_Analysis(WIDETABLEFORM_FILENAME)
         self.WideDataResults = openpyxl.load_workbook(WIDETABLEFORM_RESULTS_FILENAME)
+
+        self.MyLargeWideData = MS_Analysis(LARGE_WIDETABLEFORM_FILENAME)
+        self.LargeWideDataResults = openpyxl.load_workbook(LARGE_WIDETABLEFORM_RESULTS_FILENAME)
+
+        self.MyCompoundData = MS_Analysis(COMPOUNDTABLEFORM_FILENAME)
+        self.CompoundDataResults = openpyxl.load_workbook(COMPOUNDTABLEFORM_RESULTS_FILENAME)
 
     def test_getnormAreaTable(self):
         #Perform normalisation using ISTD
-        [norm_Area_df,ISTD_Area,ISTD_map_df,ISTD_Report] = self.MyData.get_Normalised_Area('normArea by ISTD',WIDETABLEFORM_ANNOTATION)
+        [norm_Area_df,ISTD_Area,ISTD_map_df,ISTD_Report] = self.MyWideData.get_Normalised_Area('normArea by ISTD',WIDETABLEFORM_ANNOTATION)
         right = self.sheet_to_table(self.WideDataResults,'normArea by ISTD')
         #Downcast both to the smallest numerical dtype possible
         right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
@@ -30,9 +44,35 @@ class Agilent_Test(unittest.TestCase):
         ISTD_map_df = ISTD_map_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
         pd.util.testing.assert_frame_equal(ISTD_map_df,right)
 
+        [norm_Area_df,ISTD_Area,ISTD_map_df,ISTD_Report] = self.MyLargeWideData.get_Normalised_Area('normArea by ISTD',LARGE_WIDETABLEFORM_ANNOTATION)
+        right = self.sheet_to_table(self.LargeWideDataResults,'normArea by ISTD')
+        #Downcast both to the smallest numerical dtype possible
+        right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        norm_Area_df = norm_Area_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(norm_Area_df,right)
+
+        right = self.sheet_to_table(self.LargeWideDataResults,'ISTD map')
+        #Downcast both to the smallest numerical dtype possible
+        right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        ISTD_map_df = ISTD_map_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(ISTD_map_df,right)
+
+        [norm_Area_df,ISTD_Area,ISTD_map_df,ISTD_Report] = self.MyCompoundData.get_Normalised_Area('normArea by ISTD',COMPOUNDTABLEFORM_ANNOTATION)
+        right = self.sheet_to_table(self.CompoundDataResults,'normArea by ISTD')
+        #Downcast both to the smallest numerical dtype possible
+        right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        norm_Area_df = norm_Area_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(norm_Area_df,right)
+
+        right = self.sheet_to_table(self.CompoundDataResults,'ISTD map')
+        #Downcast both to the smallest numerical dtype possible
+        right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        ISTD_map_df = ISTD_map_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(ISTD_map_df,right)
+
     def test_getAnalyteConcTable(self):
         #Perform analyte concentration using ISTD
-        [norm_Conc_df,ISTD_Conc_df,ISTD_Samp_Ratio_df,Sample_Annot_df] = self.MyData.get_Analyte_Concentration('normConc by ISTD',WIDETABLEFORM_ANNOTATION)
+        [norm_Conc_df,ISTD_Conc_df,ISTD_Samp_Ratio_df,Sample_Annot_df] = self.MyWideData.get_Analyte_Concentration('normConc by ISTD',WIDETABLEFORM_ANNOTATION)
         right = self.sheet_to_table(self.WideDataResults,'normConc by ISTD')
         #Downcast both to the smallest numerical dtype possible
         right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
@@ -45,9 +85,35 @@ class Agilent_Test(unittest.TestCase):
         Sample_Annot_df = Sample_Annot_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
         pd.util.testing.assert_frame_equal(Sample_Annot_df,right)
 
+        [norm_Conc_df,ISTD_Conc_df,ISTD_Samp_Ratio_df,Sample_Annot_df] = self.MyLargeWideData.get_Analyte_Concentration('normConc by ISTD',LARGE_WIDETABLEFORM_ANNOTATION)
+        right = self.sheet_to_table(self.LargeWideDataResults,'normConc by ISTD')
+        #Downcast both to the smallest numerical dtype possible
+        right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        norm_Conc_df = norm_Conc_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(norm_Conc_df,right)
+
+        right = self.sheet_to_table(self.LargeWideDataResults,'Sample Annot')
+        #Downcast both to the smallest numerical dtype possible
+        right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        Sample_Annot_df = Sample_Annot_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(Sample_Annot_df,right)
+
+        [norm_Conc_df,ISTD_Conc_df,ISTD_Samp_Ratio_df,Sample_Annot_df] = self.MyCompoundData.get_Analyte_Concentration('normConc by ISTD',COMPOUNDTABLEFORM_ANNOTATION)
+        right = self.sheet_to_table(self.CompoundDataResults,'normConc by ISTD')
+        #Downcast both to the smallest numerical dtype possible
+        right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        norm_Conc_df = norm_Conc_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(norm_Conc_df,right)
+
+        right = self.sheet_to_table(self.CompoundDataResults,'Sample Annot')
+        #Downcast both to the smallest numerical dtype possible
+        right = right.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        Sample_Annot_df = Sample_Annot_df.apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(Sample_Annot_df,right)
 
     def tearDown(self):
         self.WideDataResults.close()
+        self.LargeWideDataResults.close()
 
     def sheet_to_table(self,workbook,sheet_name):
         ws = workbook[sheet_name]
--- tests/test_MSRawData.py
+++ tests/test_MSRawData.py
@@ -3,41 +3,99 @@ import os
 import pandas as pd
 import openpyxl
 from MSRawData import AgilentMSRawData
+from MSRawData import SciexMSRawData
 from MSDataOutput import MSDataOutput
 
 WIDETABLEFORM_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'WideTableForm.csv')
 WIDETABLEFORM_RESULTS_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'WideTableForm_Results.xlsx')
 WIDETABLEFORM_TRANSPOSE_RESULTS_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'WideTableForm_TransposeResults.xlsx')
+
+LARGE_WIDETABLEFORM_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'sPerfect_Index_AllLipids_raw.csv')
+LARGE_WIDETABLEFORM_RESULTS_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'sPerfect_Index_AllLipids_raw_Results.xlsx')
+
 COMPOUNDTABLEFORM_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'CompoundTableForm.csv')
+COMPOUNDTABLEFORM_RESULTS_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'CompoundTableForm_Results.xlsx')
+
+SCIEX_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'Mohammed_SciEx_data.txt')
+SCIEX_RESULTS_FILENAME = os.path.join(os.path.dirname(__file__),"testdata", 'Mohammed_SciEx_data_Results.xlsx')
 
 class Agilent_Test(unittest.TestCase):
 
     def setUp(self):
         self.WideData = AgilentMSRawData(WIDETABLEFORM_FILENAME,ingui=True)
+        self.LargeWideData = AgilentMSRawData(LARGE_WIDETABLEFORM_FILENAME,ingui=True)
         self.CompoundData = AgilentMSRawData(COMPOUNDTABLEFORM_FILENAME,ingui=True)
+        self.SciexData = SciexMSRawData(SCIEX_FILENAME,ingui=True)
 
         self.WideDataResults = openpyxl.load_workbook(WIDETABLEFORM_RESULTS_FILENAME)
         self.WideDataTransposeResults = openpyxl.load_workbook(WIDETABLEFORM_TRANSPOSE_RESULTS_FILENAME)
+        self.LargeWideDataResults = openpyxl.load_workbook(LARGE_WIDETABLEFORM_RESULTS_FILENAME)
+        self.CompoundDataResults = openpyxl.load_workbook(COMPOUNDTABLEFORM_RESULTS_FILENAME)
+        self.SciexDataResults = openpyxl.load_workbook(SCIEX_RESULTS_FILENAME)
 
     def test_DataForm(self):
         self.assertEqual("WideTableForm",self.WideData.DataForm)
+        self.assertEqual("WideTableForm",self.LargeWideData.DataForm)
         self.assertEqual("CompoundTableForm",self.CompoundData.DataForm)
 
     def test_getAreaTable(self):
         pd.util.testing.assert_frame_equal(self.WideData.get_table("Area"),self.sheet_to_table(self.WideDataResults,"Area"))
         pd.util.testing.assert_frame_equal(MSDataOutput.transpose_MSdata(self.WideData.get_table("Area")),self.sheet_to_table(self.WideDataTransposeResults,"Area"))
-        #pd.util.testing.assert_frame_equal(self.CompoundData.get_table("Area"),AreaTable)
+        
+        #Downcast both to the smallest numerical dtype possible
+        right = self.sheet_to_table(self.LargeWideDataResults,"Area").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        Area_df = self.LargeWideData.get_table("Area").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(Area_df,right)
+
+        #Downcast both to the smallest numerical dtype possible
+        right = self.sheet_to_table(self.CompoundDataResults,"Area").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        Area_df = self.CompoundData.get_table("Area").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(Area_df,right)
+
+        #Downcast both to the smallest numerical dtype possible
+        right = self.sheet_to_table(self.SciexDataResults,"Area").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        Area_df = self.SciexData.get_table("Area").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(Area_df,right)
 
     def test_getRTTable(self):
         pd.util.testing.assert_frame_equal(self.WideData.get_table("RT"),self.sheet_to_table(self.WideDataResults,"RT"))
         pd.util.testing.assert_frame_equal(MSDataOutput.transpose_MSdata(self.WideData.get_table("RT")),self.sheet_to_table(self.WideDataTransposeResults,"RT"))
 
+        #Downcast both to the smallest numerical dtype possible
+        right = self.sheet_to_table(self.LargeWideDataResults,"RT").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        RT_df = self.LargeWideData.get_table("RT").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(RT_df,right)
+
+        #Downcast both to the smallest numerical dtype possible
+        right = self.sheet_to_table(self.CompoundDataResults,"RT").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        RT_df = self.CompoundData.get_table("RT").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(RT_df,right)
+
+        #Downcast both to the smallest numerical dtype possible
+        right = self.sheet_to_table(self.SciexDataResults,"RT").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        RT_df = self.SciexData.get_table("RT").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(RT_df,right)
+
     def test_getFWHMTable(self):
         pd.util.testing.assert_frame_equal(self.WideData.get_table("FWHM"),self.sheet_to_table(self.WideDataResults,"FWHM"))
         pd.util.testing.assert_frame_equal(MSDataOutput.transpose_MSdata(self.WideData.get_table("FWHM")),self.sheet_to_table(self.WideDataTransposeResults,"FWHM"))
 
+        #Downcast both to the smallest numerical dtype possible
+        right = self.sheet_to_table(self.LargeWideDataResults,"FWHM").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        FWHM_df = self.LargeWideData.get_table("FWHM").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(FWHM_df,right)
+
+        #Downcast both to the smallest numerical dtype possible
+        right = self.sheet_to_table(self.SciexDataResults,"FWHM").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        FWHM_df = self.SciexData.get_table("FWHM").apply(pd.to_numeric, errors='ignore', downcast = 'float')
+        pd.util.testing.assert_frame_equal(FWHM_df,right)
+
     def tearDown(self):
         self.WideDataResults.close()
+        self.WideDataTransposeResults.close()
+        self.LargeWideDataResults.close()
+        self.CompoundDataResults.close()
+        self.SciexDataResults.close()
 
     def sheet_to_table(self,workbook,sheet_name):
         ws = workbook[sheet_name]
